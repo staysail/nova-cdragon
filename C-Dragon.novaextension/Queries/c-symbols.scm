32e220a65ea0161778766335c633e817f86021a3
Garrett D'Amore <garrett@damore.org>
2022-12-04 16:58:11 -0800

    Fix for C symbolication.
    This does a better job of finding variable, parameter, and field
    names when declared using pointer or array syntax, and it avoids
    conflating struct/union/enum/typedef declarations with uses.

--- C-Dragon.novaextension/Queries/c-symbols.scm
+++ C-Dragon.novaextension/Queries/c-symbols.scm
@@ -39,20 +39,26 @@
 ;   (#set! arguments.query "c-arguments.scm")
 )
 
-((type_definition declarator: (type_identifier) @name) @subtree (#set! role type))
+((_ declarator: (type_identifier) @name) @subtree (#set! role type))
 
-((struct_specifier name: (type_identifier) @name) @subtree (#set! role struct))
+; struct specifier is a struct definition only if it has a body
+; we are not symbolicating anonymous structs yet (not sure how they would be useful)
+((struct_specifier name: (type_identifier) @name body: (field_declaration_list)) @subtree (#set! role struct))
 
-((enum_specifier name: (type_identifier) @name) @subtree (#set! role enum))
+((enum_specifier name: (type_identifier) @name body: (enumerator_list)) @subtree (#set! role enum))
 
 ((enum_specifier (enumerator_list (enumerator name:(identifier) @name) @subtree (#set! role enum-member))))
 
-((union_specifier name: (type_identifier) @name) @subtree (#set! role union))
+((union_specifier name: (type_identifier) @name body: (field_declaration_list)) @subtree (#set! role union))
 
 ((comment) @subtree (#set! role comment))
 
-((((_) declarator: (identifier) @name) @subtree (#set! role variable)))
-((((_) declarator: (field_identifier) @name) @subtree (#set! role property)))
+(((declaration declarator: (identifier) @name) @subtree (#set! role variable)))
+(((parameter_declaration declarator: (identifier) @name) @subtree (#set! role variable)))
+(((init_declarator declarator: (identifier) @name) @subtree (#set! role variable)))
+(((pointer_declarator declarator: (identifier) @name) @subtree (#set! role variable)))
+(((array_declarator declarator: (identifier) @name) @subtree (#set! role variable)))
+(((_ declarator: (field_identifier) @name) @subtree (#set! role property)))
 
 ; preprocessor queries
 ((preproc_def name: (identifier) @name) @subtree (#set! role constant))
